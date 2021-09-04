bf7f691128033f5b7d0b1399f81bd2bb088ab9a6
mhai <mhaikodchakorn@gmai.com>
2021-09-04 18:04:43 +0700

    [update] at DiceGame.js

--- แบบฝึกหัด_3/DiceGame.js
+++ แบบฝึกหัด_3/DiceGame.js
@@ -1,12 +1,16 @@
-
 let dice = {
     randomDice: function(){return Math.floor(Math.random() * 6)+1;}
 }
 
 
-function playGames(p1, p2, play) {
-    let p1Win=0;
-    let p2Win=0;
+function playGames(p1, p2, playRound) {
+    if(playRound==0){
+        return `Please enter another number
+        `;
+    }
+
+    let countP1Win=0;
+    let countP2Win=0;
     let round=1;
 
     do{
@@ -20,31 +24,34 @@ function playGames(p1, p2, play) {
             person.player1.result = "Draw";
             person.player2.result = "Draw";
             console.log(person);
-
     } 
     else if (person.player1.roll > person.player2.roll) {
         person.player1.result = "WIN";
         person.player2.result = "LOSE";
         console.log(person);
-        p1Win++;
+        countP1Win++;
     } else if (person.player1.roll < person.player2.roll) {
         person.player1.result = "LOSE";
         person.player2.result = "WIN";
         console.log(person);
-        p2Win++;
+        countP2Win++;
     }
-    play--;
-}while(play>0);
-    if(p1Win==p2Win){
-        return `Drow`;
-    }else if(p1Win>p2Win){
-        return`${p1} Winner ${p1Win} Round!!! `;
+    playRound--;
+}while(playRound>0);
+    if(countP1Win==countP2Win){
+        return `Draw
+        `;
+    }else if(countP1Win>countP2Win){
+        return`${p1} Winner ${countP1Win} Round!!!
+        `;
     }else {
-        return`${p2} Winner ${p2Win} Round!!!`;
+        return`${p2} Winner ${countP2Win} Round!!!
+        `;
     }
 }
 
-console.log(playGames("Alice", "Bob", 5));
-// console.log(playGames("Prew", "Mhai", 5));
-// console.log(playGames("Fahh", "Yok", 5));
-console.log(typeof(dice));
+console.log(playGames("Alice", "Bob", 5)); 
+console.log(playGames("Prew", "Mhai", 7)); 
+console.log(playGames("Fahh", "Yok", 6)); 
+
+console.log(playGames("JameKung", "PungKung", 0));
